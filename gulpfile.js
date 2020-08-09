a6f3457a10b502bc47dded49a4e9d241ae08fd01
Amir Nabeebacus <amir@nleurope.com>
2020-08-09 09:59:30 +0400

    gulp v4 setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const sass = require( 'gulp-sass' );
 const autoprefixer = require( 'autoprefixer' );
 const postcss = require( 'gulp-postcss' );
 const cssnano = require( 'cssnano' );
+const cleanCSS = require('gulp-clean-css');
 
 // JS related plugins
 const uglify = require( 'gulp-uglify' );
@@ -23,6 +24,16 @@ const buffer = require( 'vinyl-buffer' );
 // Utility related plugins
 const rename = require( 'gulp-rename' );
 const sourcemaps = require( 'gulp-sourcemaps' );
+const plumber = require( 'gulp-plumber' );
+const del = require('del');
+let replace = require('gulp-replace');
+
+// Image related plugins
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+
+// Browers related plugins
+const browserSync = require( 'browser-sync' ).create();
 
 sass.compiler = require( 'node-sass' );
 
@@ -31,22 +42,46 @@ sass.compiler = require( 'node-sass' );
 const path = {
     css: {
       src: [
-        'node_modules/@fortawesome/fontawesome-free/scss/fontawesome.scss',
         'node_modules/bootstrap/scss/bootstrap.scss',
-        'app/scss/**/*.scss'
+        './app/scss/**/*.scss'
       ],
-      dist: 'dist/css',
-      cssFolder: 'app/css/'
+      dist: './dist/css',
+      cssFolder: './app/css/'
     },
     js: {
-      src: 'app/js/**/*.js',
-      dist: 'dist/js',
+      src: './app/js/**/*.js',
+      dist: './dist/js',
       jsFiles: [
         'script.js'
       ],
-      jsFolder: 'app/js/'
+      jsFolder: './app/js/'
     },
-    mapURL: './'
+    mapURL: './',
+    fonts: {
+      src: './app/fonts/**/*',
+      dist: './dist/fonts'
+    },
+    images: {
+      src: './app/images/**/*.+(png|jpg|jpeg|gif|svg)',
+      dist: './dist/images'
+    },
+    html: './**/*.html'
+}
+
+/**
+ * Browser-sync
+ */
+function browserSyncTask() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+}
+
+function reload(done) {
+	browserSync.reload();
+	done();
 }
 
 /**
@@ -55,24 +90,45 @@ const path = {
 function styleTask(done) {
   src( path.css.src )
     .pipe( sourcemaps.init() )
-    .pipe( sass( {outputStyle: 'compressed'} ).on( 'error', sass.logError ) )
+    .pipe( sass({
+      errLogToConsole: true,
+      outputStyle: 'compressed'
+    })
+    .on( 'error', sass.logError ) )
     .pipe( postcss( [
       autoprefixer(),
       cssnano()
     ]) )
     .pipe( rename( {suffix: '.min'} ) )
     .pipe(sourcemaps.write( path.mapURL ))
-    .pipe( dest( path.css.dist ) );
+    .pipe( dest( path.css.dist ) )
+    .pipe( browserSync.stream() );
   done();
+}
+
+/**
+ * Plumber
+ */
+function triggerPlumber( src_file, dest_file ) {
+	return src( src_file )
+		.pipe( plumber() )
+		.pipe( dest( dest_file ) );
 }
 
 /**
  * Move css vendor plugins to dist/vendor
  */
-function cssVendorTask(done) {
-  src(path.css.cssFolder + 'vendor/**/*.css')
-    .pipe( dest( path.css.dist + '/vendor' ) );
-  done();  
+function cssVendorTask() {
+  const cssSRC = path.css.cssFolder + 'vendor/**/*.css';
+  const vendorDIST = path.css.dist + '/vendor';
+
+  return src( cssSRC )
+          .pipe( rename({ extname: '.min.css' }) )
+          .pipe(cleanCSS({debug: true}, (details) => {
+            console.log(`${details.name}: ${details.stats.originalSize}`);
+            console.log(`${details.name}: ${details.stats.minifiedSize}`);
+          }))
+          .pipe( dest( vendorDIST ) ); 
 }
 
 /**
@@ -92,6 +148,7 @@ function jsTask(done) {
         .pipe( uglify() )
         .pipe( sourcemaps.write( path.mapURL ) )
         .pipe( dest( path.js.dist ) )
+        .pipe( browserSync.stream() );
     });
     done();
 }
@@ -99,12 +156,66 @@ function jsTask(done) {
 /**
  * Move jss vendor plugins to dist/vendor
  */
-function jsVendorTask(done) {
-    src(path.js.jsFolder + 'vendor/**/*.js')
-      .pipe( rename({ extname: '.min.js' }) )
-      .pipe( uglify() )
-      .pipe( dest( path.js.dist + '/vendor' ) );
-    done();  
+function jsVendorTask() {
+  const jsSRC = path.js.jsFolder + 'vendor/**/*.js';
+  const vendorDIST = path.js.dist + '/vendor';
+
+  return src( jsSRC )
+          .pipe( rename({ extname: '.min.js' }) )
+          .pipe( uglify() )
+          .pipe( dest( vendorDIST ) );
+}
+
+/**
+ * Move fonts files to dist folder
+ */
+function fontsTask() {
+	return triggerPlumber( path.fonts.src, path.fonts.dist );
+};
+
+/**
+ * Move fonts files to dist folder
+ */
+function imageminTask(done) {
+	src( path.images.src )
+    .pipe(cache( imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.mozjpeg({quality: 50, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ]) ))
+    .pipe(dest( path.images.dist ))
+
+  done();
+};
+
+// Cachebust
+function cacheBustTask() {
+  var cbString = new Date().getTime();
+  return src(['index.html'])
+      .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+      .pipe(dest('.'));
+}
+
+/**
+ * Clean dist folder
+ */
+async function cleanTask() {
+	const deletedPaths = await del(['./dist']);
+ 
+  console.log('Deleted files and directories:\n', deletedPaths.join('\n'));
+};
+
+/**
+ * Clear cache
+ */
+async function clearCache() {
+    cache.clearAll();
 }
 
 
@@ -112,18 +223,44 @@ function jsVendorTask(done) {
  * Watch task
  */
 function watchTask() {
-  watch( path.css.src, {usePolling : true}, series( styleTask ) );
-  watch( path.js.src, {usePolling : true}, series( jsTask ) );
+  watch( path.css.src, {usePolling : true}, series( styleTask, cacheBustTask, reload ) );
+  watch( path.js.src, {usePolling : true}, series( jsTask, cacheBustTask, reload ) );
+  watch(
+    [path.fonts.src, path.images.src, path.html],
+    {usePolling : true},
+    series( fontsTask, imageminTask, reload )
+  );
 }
 
-// exports.styleTask = styleTask;
-// exports.jsTask = jsTask;
-// exports.jsVendorTask = jsVendorTask;
-// exports.cssVendorTask = cssVendorTask;
+/**
+ * Clean task
+ * to clean/remove the dist folder
+ * Run "gulp cleanTask" on your terminal
+ */
+exports.cleanTask = cleanTask;
+
+/**
+ * Clear cache task
+ * to clear the cache for example: the images cached
+ * Run "gulp clearCache" on your terminal
+ */
+exports.clearCache = clearCache;
+
+/**
+ * Watch task
+ * Run "gulp watch" on your terminal
+ */
 exports.watch = watch;
 
+/**
+ * Default task
+ * Run "gulp" on your terminal
+ */
 exports.default = series(
+  parallel(cleanTask, clearCache),
+  fontsTask,
+  imageminTask,
   parallel(styleTask, cssVendorTask),
   parallel(jsTask, jsVendorTask),
-  watchTask
+  parallel(browserSyncTask, cacheBustTask, watchTask)
 );
